143931288a26ffe6db6e04c7adffaec4d61238cf
Alexander <alex.albasov@gmail.com>
2023-08-02 20:11:20 +0700

    add .appvoyer.yml and change some methods

--- src/test/java/steps/TemplateSteps.java
+++ src/test/java/steps/TemplateSteps.java
@@ -16,37 +16,41 @@ public class TemplateSteps {
     private static VerificationPage verificationPage;
     private static TransferPage transferPage;
 
-    @Пусть("открыта страница с формой авторизации {String}")
+    @Пусть("открыта страница с формой авторизации {string}")
     public void openAuthPage(String url) {
         loginPage = Selenide.open(url, LoginPage.class);
     }
 
-    @Когда("пользователь пытается авторизоваться с именем {String} и паролем {String}")
+    @Когда("пользователь пытается авторизоваться с именем {string} и паролем {string}")
     public void loginWithNameAndPassword(String login, String password) {
         verificationPage = loginPage.validLogin(login, password);
     }
 
-    @И("пользователь вводит проверочный код из смс {String}")
+    @И("пользователь вводит проверочный код из смс {string}")
     public void setValidCode(String verificationCode) {
         dashboardPage = verificationPage.validVerify(verificationCode);
     }
 
-    @И("пользователь нажимает пополнить баланс карты {String}")
+    @И("пользователь нажимает пополнить баланс карты {string}")
     public void selectCardToTransfer(String cardNumber) {
         transferPage = dashboardPage.selectCardToTransfer(cardNumber);
     }
 
-    @И("пользователь делает перевод с карты {String} на сумму {String}")
+    @И("пользователь делает перевод с карты {string} на сумму {string}")
     public void makeTransfer(String cardNumber, String amount) {
         dashboardPage = transferPage.makeValidTransfer(amount, cardNumber);
     }
 
-    @Тогда("пользователь попадает на страницу со списком карт.На первой карте {String}, на второй {String}")
+    @Тогда("пользователь попадает на страницу со списком карт.На первой карте {string}, на второй {string}")
     public void shouldHaveSuccessTransfer(String expectedBalanceOfFirstCard, String expectedBalanceOfSecondCard) {
         var actualBalanceOfFirstCard = dashboardPage.getCardBalance(DataHelper.getFirstCardInfo().getCardNumber());
         var actualBalanceOfSecondCard = dashboardPage.getCardBalance(DataHelper.getSecondCardInfo().getCardNumber());
-        assertEquals(expectedBalanceOfFirstCard, actualBalanceOfFirstCard);
-        assertEquals(expectedBalanceOfSecondCard, actualBalanceOfSecondCard);
+        System.out.println(expectedBalanceOfFirstCard);
+        System.out.println(expectedBalanceOfSecondCard);
+        System.out.println(actualBalanceOfFirstCard);
+        System.out.println(actualBalanceOfSecondCard);
+        assertEquals(Integer.parseInt(expectedBalanceOfFirstCard), actualBalanceOfFirstCard);
+        assertEquals(Integer.parseInt(expectedBalanceOfSecondCard), actualBalanceOfSecondCard);
     }
 
 }
